c83afcb7089908c46d3c789d0a0ec759cdc959ae
Михаил Свинцицкий <starenginegames@gmail.com>
2024-07-28 21:23:47 +1000

    Добавил планету, гравитацию, валли с анимой, скайбокс

---
+++ TerraForm/Source/TerraForm/PlanetaryMovementComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PlanetaryMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRAFORM_API UPlanetaryMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+public: 
+	UFUNCTION(Blueprintcallable)
+		void UpdateGravityDirection(const FVector& NewGravityDir);
+
+		virtual bool FloorSweepTest(
+			struct FHitResult& OutHit,
+			const FVector& Start,
+			const FVector& End,
+			ECollisionChannel TraceChannel,
+			const struct FCollisionShape& CollisionShape,
+			const struct FCollisionQueryParams& Params,
+			const struct FCollisionResponseParams& ResponseParam
+		) const override;
+};
